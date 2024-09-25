059262b0d862200c18249c8fb6d6a90f980943af
JinJungs <nehciyz@gmail.com>
2024-09-25 23:32:35 +0900

    add a solution of 21

---
+++ linkedList/easy/21. Merge Two Sorted Lists.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1 and not list2:
+            return None
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+
+        # Define dummy head makes code clean!!
+        head = ListNode()
+        head1 = list1
+        head2 = list2
+
+        result = head
+        while head1 and head2:
+            if head1.val < head2.val:
+                head.next = head1
+                head1 = head1.next
+            else:
+                head.next = head2
+                head2 = head2.next
+            head = head.next
+
+        if head1 and not head2:
+            head.next = head1
+        elif head2 and not head1:
+            head.next = head2
+
+        return result.next
+
+
+
+
+
