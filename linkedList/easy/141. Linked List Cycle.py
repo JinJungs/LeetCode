5ec319cdd758ea9aa231407128dc5ae625b3242f
JinJungs <nehciyz@gmail.com>
2024-09-25 23:31:54 +0900

    add a solution of 141

---
+++ linkedList/easy/141. Linked List Cycle.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        l = set()
+        while head:
+            val = head.val
+            tail = head.next
+
+            if head in l:
+                return True
+
+            l.add(head)
+            head = tail
+
+        return False
+
+# Floyd’s Cycle Detection Algorithm
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow_pointer = head
+        fast_pointer = head
+        while fast_pointer and fast_pointer.next:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+            if slow_pointer == fast_pointer:
+                return True
+        return False
