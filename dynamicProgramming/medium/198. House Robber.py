8bc9f9d2d9868ef99082bf75fdd93d632b4c814d
JinJungs <nehciyz@gmail.com>
2024-09-29 23:51:24 +0900

    add a solution of 198 from LeetCode

--- dynamicProgramming/medium/198. House Robber.py
+++ dynamicProgramming/medium/198. House Robber.py
@@ -17,3 +17,22 @@ class Solution:
             return d[start]
 
         return max(recursive(0), recursive(1))
+
+
+    # Solution From LeetCode
+    def rob2(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        if n == 1:
+            return nums[0]
+
+        # dp start index with 0
+        dp = [0] * n
+
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+
+        return dp[-1]
