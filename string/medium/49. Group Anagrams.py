663f30198fc050a6cc944ab76bbe391e2605b08a
JinJungs <nehciyz@gmail.com>
2024-10-05 23:45:40 +0900

    fix a solution of 49

---
+++ string/medium/49. Group Anagrams.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if not strs:
+            return [[""]]
+
+        """
+        should be equal
+        # sorted list
+        """
+        d = dict()
+        for s in strs:
+            t = str(sorted(s))
+            if t in d.keys():
+                d[t].append(s)
+            else:
+                d[t] = [s]
+
+        return [x for x in d.values()]
