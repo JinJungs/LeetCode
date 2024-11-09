fd77aaffd016d0e2e36f82c7a6a28e6c2f51dd5c
JinJungs <nehciyz@gmail.com>
2024-11-09 22:33:16 +0900

    add a solution of 1971 - with DFS

--- unionFind/1971. Find if Path Exists in Graph.py
+++ unionFind/1971. Find if Path Exists in Graph.py
@@ -2,6 +2,7 @@ from typing import List
 
 
 class Solution:
+    # UNION FIND
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         if not edges:
             return True
@@ -38,3 +39,38 @@ class Solution:
             union(edge[0], edge[1])
 
         return find(source) == find(destination)
+
+
+    # DFS
+    def validPath2(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        if not edges:
+            return True
+
+        # make dictionary with key: node , value: linked node list
+        d = dict()
+        for edge in edges:
+            v1 = edge[0]
+            v2 = edge[1]
+
+            if v1 in d.keys():
+                d[v1].append(v2)
+            else:
+                d[v1] = [v2]
+
+            if v2 in d.keys():
+                d[v2].append(v1)
+            else:
+                d[v2] = [v1]
+
+        visited = [False] * n
+        def dfs(s):
+            visited[s] = True
+
+            for v in d[s]:
+                if v == destination:
+                    return True
+                if not visited[v] and dfs(v):
+                    return True
+            return False
+
+        return dfs(source)
